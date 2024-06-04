9592397f53c72e1c336035539b6f0da09f8e44c1
Pandora10025 <98975655+Pandora10025@users.noreply.github.com>
2024-06-04 17:12:20 -0400

    It's been 4 hours
    I worked on a couple of things, added a movement script and new objects to the project, set up the back drops for the Auris region.

---
+++ Three Kingdoms Flatgame/Assets/Player_Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // player Icons 
+    public SpriteRenderer player_icon;
+    public SpriteRenderer tenga_icon;
+    public SpriteRenderer solas_icon;
+    public SpriteRenderer auris_icon;
+
+    Vector3 startPos;
+      
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = new Vector3(17f, -7f, -1f);
+        this.transform.position = startPos;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.localPosition += new Vector3(0,0.01f, 0);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.localPosition += new Vector3(-0.01f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.localPosition += new Vector3(0.01f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.localPosition += new Vector3(0, -0.01f, 0);
+        }
+    }
+}
