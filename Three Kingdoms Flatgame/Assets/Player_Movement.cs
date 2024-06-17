36a6edb690fcf3d87144d67c163971114f3eb110
Pandora10025 <98975655+Pandora10025@users.noreply.github.com>
2024-06-17 05:37:52 -0400

    Triggers, Sprites and Audio Work
    Fixed trigger issues so sprite changes now occur. Audio now in game!

--- Three Kingdoms Flatgame/Assets/Player_Movement.cs
+++ Three Kingdoms Flatgame/Assets/Player_Movement.cs
@@ -11,14 +11,26 @@ public class Player_Movement : MonoBehaviour
     public Sprite tenga_icon;
     public Sprite solas_icon;
     public Sprite auris_icon;
+    public Sprite original;
 
     // starting position
     Vector3 startPos;
 
-    // Kingdom Boundary 
-   // public Collider2D solasBoundary;
-   // public Collider2D aurisBoundary;
-   // public Collider2D tengaBoundary;
+    //Audio Clips
+    public AudioClip _forest;
+    public AudioClip _farm;
+    public AudioClip _coast;
+    public AudioClip _beach;
+    public AudioClip _arena;
+    public AudioClip _market;
+
+    //Audio Sources
+    public AudioSource forest;
+    public AudioSource coast;
+    public AudioSource beach;
+    public AudioSource farm;
+    public AudioSource arena;
+    public AudioSource market;
 
     //Bools
     bool inAuris = false;
@@ -28,6 +40,7 @@ public class Player_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         startPos = new Vector3(17f, -7f, -1f);
         this.transform.position = startPos;
 
@@ -40,37 +53,39 @@ public class Player_Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
         {
-            this.transform.localPosition += new Vector3(0,0.01f, 0);
+            this.transform.localPosition += new Vector3(0,0.05f, 0);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.localPosition += new Vector3(-0.01f, 0, 0);
+            this.transform.localPosition += new Vector3(-0.05f, 0, 0);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.localPosition += new Vector3(0.01f, 0, 0);
+            this.transform.localPosition += new Vector3(0.05f, 0, 0);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            this.transform.localPosition += new Vector3(0, -0.01f, 0);
+            this.transform.localPosition += new Vector3(0, -0.05f, 0);
         }
 
         // sprite and music changes 
         
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collision.gameObject.CompareTag("solas_boundary"))
+
+        if (collider.gameObject.CompareTag("solas_boundary"))
         {
             inSolas = true;
             inAuris = false;
             inTenga = false;
 
-            GetComponent<SpriteRenderer>().sprite = solas_icon;
-            Debug.Log("in Solas");
-        }
+            Debug.Log("In Solas");
 
-        if(collision.gameObject.CompareTag("auris_boundary"))
+            GetComponent<SpriteRenderer>().sprite = solas_icon;
+            
+        }
+        else if (collider.gameObject.CompareTag("auris_boundary"))
         {
             inAuris = true;
             inSolas = false;
@@ -78,8 +93,7 @@ public class Player_Movement : MonoBehaviour
 
             GetComponent<SpriteRenderer>().sprite = auris_icon;
         }
-
-        if (collision.gameObject.CompareTag("tenga_boundary"))
+        else if (collider.gameObject.CompareTag("tenga_boundary"))
         {
             inTenga = true;
             inAuris = false;
@@ -87,6 +101,79 @@ public class Player_Movement : MonoBehaviour
 
             GetComponent<SpriteRenderer>().sprite = tenga_icon;
         }
+        else if (collider.gameObject.CompareTag("open_boundary"))
+        {
+            inAuris = false;
+            inSolas = false;
+            inTenga =false;
+
+            GetComponent<SpriteRenderer>().sprite = original;
+        }
+
+
+        // solas audio triggers
+        if (collider.gameObject.CompareTag("solas_market"))
+        {
+            Debug.Log("In Market");
+            market.PlayOneShot(_market, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("farm"))
+        {
+            farm.PlayOneShot(_farm, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("forest"))
+        {
+            forest.PlayOneShot(_forest, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("open_boundary") || collider.gameObject.CompareTag("tenga_boudary") || collider.gameObject.CompareTag("auris_boundary"))
+        {
+            market.Stop();
+            farm.Stop();
+            forest.Stop();
+        }
+
+        // tenga audio triggers
+        if(collider.gameObject.CompareTag("tenga_market"))
+        {
+            Debug.Log("In Market");
+            market.PlayOneShot(_market, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("beach"))
+        {
+            beach.PlayOneShot(_beach, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("coast"))
+        {
+            coast.PlayOneShot(_coast, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("open_boundary") || collider.gameObject.CompareTag("solas_boudary") || collider.gameObject.CompareTag("auris_boundary"))
+        {
+            market.Stop();
+            farm.Stop();
+            forest.Stop();
+        }
+
+        // auris audio triggers
+        if(collider.gameObject.CompareTag("auris_market"))
+        {
+            Debug.Log("In Market");
+            market.PlayOneShot(_market, 0.75f);
+        }
+        else if (collider.gameObject.CompareTag("arena"))
+        {
+            arena.PlayOneShot(_arena, 0.75f);
+        }
+       /* else if (collider.gameObject.CompareTag("forest"))
+        {
+            forest.PlayOneShot(_forest, 0.75f);
+        }*/
+        else if (collider.gameObject.CompareTag("open_boundary") || collider.gameObject.CompareTag("tenga_boudary") || collider.gameObject.CompareTag("solas_boundary"))
+        {
+            market.Stop();
+            farm.Stop();
+            forest.Stop();
+        }
+
     }
 }
 
